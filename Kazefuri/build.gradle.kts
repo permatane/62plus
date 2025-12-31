d472f87a1cc6ab371c590a336d132dde04416438
Matane <permatamailbox@gmail.com>
2025-12-31 15:33:31 +0700

    Update build.gradle.kts

--- Kazefuri/build.gradle.kts
+++ Kazefuri/build.gradle.kts
@@ -1,36 +1,36 @@
 plugins {
-    id 'com.android.library'
-    id 'org.jetbrains.kotlin.android'
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    compileSdk 34
-    
+    namespace = "com.kazefuri"
+    compileSdk = 34
+
     defaultConfig {
-        minSdk 21
-        targetSdk 34
+        minSdk = 21
     }
-    
+
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
-    
+
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-    
+
     kotlinOptions {
-        jvmTarget = '1.8'
+        jvmTarget = "1.8"
     }
 }
 
 dependencies {
-    implementation 'com.lagradost:cloudstream3:4.6.0'
-}
-
-    iconUrl = "https://www.google.com/s2/favicons?domain=sv3.kazefuri.cloud&sz=%size%"
+    implementation("com.lagradost:cloudstream3:4.6.0")
 }
