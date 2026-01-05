fe0057b896f72ef937a3c01db8aa4fefa7a2d7c4
Matane <permatamailbox@gmail.com>
2026-01-05 12:14:54 +0700

    Add files via upload

---
+++ Idlix/build.gradle.kts
@@ -0,0 +1,15 @@
+version = 3
+
+cloudstream {
+    language = "id"
+    description = "Idlix - Movie subtitle indonesia"
+    authors = listOf("Matane, Hexated, Phisher98")
+	isCrossPlatform = true
+    status = 1
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+        "Anime",
+        "AsianDrama",
+    )
+}
---
+++ Idlix/src/main/kotlin/com/Idlix/IdlixPlugin.kt
@@ -0,0 +1,12 @@
+package com.Idlix
+
+import com.lagradost.cloudstream3.plugins.BasePlugin
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+
+@CloudstreamPlugin
+class IdlixPlugin: BasePlugin() {
+    override fun load() {
+        registerMainAPI(Idlix())
+        registerExtractorAPI(Jeniusplay())
+    }
+}
