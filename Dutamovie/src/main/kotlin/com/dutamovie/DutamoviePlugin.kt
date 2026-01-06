6a2ab670d9f964503a026f2d0c7f37cc91f6c954
Matane <permatamailbox@gmail.com>
2026-01-06 15:00:22 +0700

    Update DutamoviePlugin.kt

--- Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
+++ Dutamovie/src/main/kotlin/com/dutamovie/DutamoviePlugin.kt
@@ -7,7 +7,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 @CloudstreamPlugin
 class DutaMoviePlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(DutaMovie())
+        registerMainAPI(dutaMovie())
         registerExtractorAPI(Dingtezuni())
         registerExtractorAPI(Bingezove())
         registerExtractorAPI(Mivalyo())
