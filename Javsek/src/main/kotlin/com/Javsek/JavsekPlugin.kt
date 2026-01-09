019aaf27818c26675a48f01e69c3b80bc9c905fe
Matane <permatamailbox@gmail.com>
2026-01-09 10:25:15 +0700

    Update JavsekPlugin.kt

--- Javsek/src/main/kotlin/com/Javsek/JavsekPlugin.kt
+++ Javsek/src/main/kotlin/com/Javsek/JavsekPlugin.kt
@@ -1,29 +1,18 @@
-package com.Javsek
+ackage com.Jasek
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+import com.lagradost.cloudstream3.extractors.StreamTape
 
 @CloudstreamPlugin
 class JavsekPlugin: Plugin() {
-    override fun load() {
+    override fun load(context: Context) {
         registerMainAPI(Javsek())
-        registerExtractorAPI(Dingtezuni())
-        registerExtractorAPI(Bingezove())
-        registerExtractorAPI(Mivalyo())
-        registerExtractorAPI(Hglink())
-        registerExtractorAPI(Ryderjet())
-        registerExtractorAPI(Ghbrisk())
-        registerExtractorAPI(Dhcplay())
-        registerExtractorAPI(Movearnpre())
-        registerExtractorAPI(Streamcasthub())
-        registerExtractorAPI(Dm21upns())
-        registerExtractorAPI(Dm21())
-        registerExtractorAPI(Dintezuvio())
-        registerExtractorAPI(Dm21embed())
-        registerExtractorAPI(Veev())
-        registerExtractorAPI(Minochinos())
+        registerExtractorAPI(StreamTape())
+        registerExtractorAPI(Stbturbo())
+        registerExtractorAPI(Turbovid())
+        registerExtractorAPI(MyCloudZ())
+        registerExtractorAPI(Cloudwish())
     }
 }
-
-
