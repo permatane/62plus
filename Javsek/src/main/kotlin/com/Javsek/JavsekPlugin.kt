6bb6744abb78679aaa743245ed0057486fc88b22
Matane <permatamailbox@gmail.com>
2026-01-09 20:13:14 +0700

    Update JavsekPlugin.kt

--- Javsek/src/main/kotlin/com/Javsek/JavsekPlugin.kt
+++ Javsek/src/main/kotlin/com/Javsek/JavsekPlugin.kt
@@ -4,10 +4,6 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 import com.lagradost.cloudstream3.extractors.StreamTape
-import com.lagradost.cloudstream3.utils.loadExtractor
-import com.lagradost.cloudstream3.utils.fixUrlNull
-import com.lagradost.cloudstream3.SubtitleFile
-import com.lagradost.cloudstream3.ExtractorLink
 
 @CloudstreamPlugin
 class JavsekPlugin: Plugin() {
