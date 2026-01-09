86a47f69879d3bc568bc135cb98ed928133f9dc5
Matane <permatamailbox@gmail.com>
2026-01-09 12:29:03 +0700

    Update Podjav.kt

--- Podjav/src/main/kotlin/com/Podjav/Podjav.kt
+++ Podjav/src/main/kotlin/com/Podjav/Podjav.kt
@@ -57,9 +57,16 @@ class Podjav : MainAPI() {
     override suspend fun load(url: String): LoadResponse {
         val document = app.get(url).document
 
-        val title = document.selectFirst("meta[property=og:title]")?.text()?.trim() ?: "Untitled"
-        val poster = fixUrlNull(document.selectFirst("meta[property=og:image]") ?.attr("content")
-        val description = document.selectFirst("meta[property=og:description]") ?.attr("content")
+        val title = document.selectFirst("meta[property=og:title]")
+            ?.attr("content")
+            ?.trim()
+            ?: "Podjav Video"
+
+        val poster = document.selectFirst("meta[property=og:image]")
+            ?.attr("content")
+
+        val description = document.selectFirst("meta[property=og:description]")
+            ?.attr("content")
 
         return newMovieLoadResponse(title, url, TvType.NSFW, url) {
             this.posterUrl = poster
@@ -113,6 +120,7 @@ class Podjav : MainAPI() {
         return edoceD
     }
 }
+
 
 
 
